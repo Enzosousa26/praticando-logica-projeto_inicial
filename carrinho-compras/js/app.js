06b8d1be6caa100b43c8a5bdaa7ad5984bc74293
Enzosousa26 <sousaenzo53@gmail.com>
2026-04-30 12:54:50 -0300

    Exercicio-Jogo

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,36 @@
+let subTotal = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').innerHTML = 'R$ 0';
+
+function adicionar(){
+    //recuperar valores nome do produto, quantidade e valor
+    //calcular o preço, o nosso subtotal
+    //adicionar no carrinho
+    //atualizar o valor total
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * valorUnitario;
+
+    let listaProdutos = document.getElementById('lista-produtos');
+    listaProdutos.innerHTML =  listaProdutos.innerHTML + `<section class="carrinho__produtos__produto"><span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span></section>`;
+
+    subTotal = subTotal + preco;
+
+    let resultado = document.getElementById('valor-total');
+    resultado.innerHTML = `<span class="texto-azul" id="valor-total">R$${subTotal}</span>`;
+
+    document.getElementById('quantidade').value = '';
+    
+}
+
+function limpar(){
+    document.getElementById('produto').value = '';
+    document.getElementById('quantidade').value = '';
+    let subTotal = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = '';
+
+
+}
